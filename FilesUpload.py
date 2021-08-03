13e0d0c3376180405f0090e30cec4830583a4302
fahimSiam <siamfahim02@gmail.com>
2021-08-03 15:39:19 +0600

    done fileUpload

--- FilesUpload.py
+++ FilesUpload.py
@@ -10,4 +10,5 @@ driver.implicitly_wait(5)
 driver.maximize_window()
 driver.get("https://cgi-lib.berkeley.edu/ex/fup.html")
 
-driver.find_element(By.name, 'upfile').send_keys('C:\Users\My Baby\Desktop\Documentation.docx')
+driver.find_element(By.NAME, 'upfile').send_keys('D:\Md Fahim Siam_Resume.pdf')
+driver.find_element(By.XPATH,'//input[@type="submit"]').click()
