88dcd98d4cb828218d1a3e343a003ca7c24d580b
fahimSiam <siamfahim02@gmail.com>
2021-08-03 09:55:40 +0600

    3/08/2021

---
+++ AuthenticationPopUpHandle.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver import ActionChains
+
+driver=webdriver.Chrome(ChromeDriverManager().install())
+
+driver.implicitly_wait(5)
+driver.maximize_window()
+driver.get("http://londonfreelance.org/courses/frames/index.html")
+#time.sleep(3)
