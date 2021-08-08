d8c69b52be4c3feb2cedcb0f594118836dcdef26
fahimSiam <siamfahim02@gmail.com>
2021-08-08 15:18:02 +0600

    8/8/21 tasks

--- OpenPyXL/OpenPyXlBasics.py
+++ OpenPyXL/OpenPyXlBasics.py
@@ -1,11 +1,35 @@
 import openpyxl
 from openpyxl import Workbook, load_workbook
-
-path = "D:\\files\\codes\\vs\\selenium\\Sample.xlsx"
-wb=load_workbook(path)
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import Font
+wb=load_workbook('Sample.xlsx')
 ws=wb.active
 cell = ws.cell(row = 1, column = 1) 
-ws['A4']='2345'
+ws['A4']='7676'
 print(ws['A4'].value)
 print(wb.sheetnames)
-#wb.save(filename='Sample.xlsx')
+wb.save(filename='Sample.xlsx')
+
+#PRINTING WHOLE DATA
+for row in range(1,10):
+    s=""
+    for col in range(1,6):
+        char=get_column_letter(col)
+        s+=(str(ws[char+str(row)].value))
+        s+=" "
+    print(s)
+
+
+#MERGING CELLS
+ws.merge_cells("A2:A3")
+ws.unmerge_cells("A2:A3")
+
+
+
+#INSERT
+ws.insert_cols(3)
+ws.delete_cols(3)
+
+ws['B2'].font= Font(bold=True, color="0099CCFF")
+
+wb.save(filename='Sample.xlsx')
